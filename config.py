6db61a4a68d3a1f8cf2db296e3cf2694be0629f9
Sam Sepiol <samsepiolhack@gmail.com>
2021-02-06 18:09:25 -0500

    Se añade configuración para generación de email mediante variables de entorno y configuración por defecto como son puerto y servidor smtp para pruebas de confirmación de cuentas

--- config.py
+++ config.py
@@ -2,6 +2,16 @@ import os
 
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'dev'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+        ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
     @staticmethod
     def init_app(app):
         pass
