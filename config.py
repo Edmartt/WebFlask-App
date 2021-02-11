5e155eb0640bf133888bd5a5468081e61116857f
Sam Sepiol <samsepiolhack@gmail.com>
2021-02-11 04:03:49 -0500

    Se hace pequeña correción de los datos de configuración por defecto para el envío del correo

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ class Config:
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKER]'
+    FLASKY_MAIL_SENDER = 'Flasker Admin <flasker@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKER_ADMIN')
 
     @staticmethod
     def init_app(app):
